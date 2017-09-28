5d23befc5067fa718b480e1c747515d319765f03
Russell Patterson <rpatterson@hmbnet.com>
2017-09-27 23:38:29 -0400

    Install script updates.

--- config/install.sh
+++ config/install.sh
@@ -2,7 +2,7 @@ cd ..
 sudo dotnet publish
 sudo mkdir -p /opt/jimbook
 sudo cp -R bin/Debug/netcoreapp2.0/publish/* /opt/jimbook/
-sudo mv config/nginx/default /etc/nginx/sites-available/
+sudo cp config/nginx/default /etc/nginx/sites-available/
 sudo cp config/systemd/jimbook.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable jimbook
